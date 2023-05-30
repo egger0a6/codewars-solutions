213c4f866abd4d216746be9066a19c0fe748e548
Zach Eggert <egger0a6@email.arizona.edu>
2023-05-29 22:25:23 -0400

    change fn to test p(n) = 0 if n - pentagonal number < 0

--- 4kyu/explosive_sum.js
+++ 4kyu/explosive_sum.js
@@ -7,18 +7,30 @@ function sum(num) {
   let partitions = [1n];
   for (let i = 1n; i <= num; i++) {
     partitions.push(0n);
+    console.log("i: " + i)
     for (let j = 1n; j <= i; j++) {
+      console.log()
+      console.log("j: " + j)
       let coeff = (-1n) ** (j + 1n);
-      for (const el of [genPentNum(j), genPentNum(-j)]) {
-        if (i - el >= 0n) {
-          partitions[i] = partitions[i] + coeff*partitions[i-el];
+      console.log("coeff " + coeff)
+      console.log("partitons " + partitions)
+      console.log([genPentNum(j), genPentNum(-j)])
+      let pentNumPos = i - genPentNum(j);
+      let pentNumNeg = i - genPentNum(-j);
+      if (pentNumPos < 0 || pentNumNeg < 0) break;
+      for (const pNum of [pentNumPos, pentNumNeg]) {
+        console.log("i-Pnum = " + (i - pNum))
+        if (pNum >= 0n) {
+          partitions[i] = partitions[i] + coeff*partitions[pNum];
+          //console.log(partitions)
         }
       }
     }
+    console.log()
   }
   return partitions[num];
 }
 
-console.log(sum(4))
+// console.log(sum(4))
 console.log(sum(10))
-console.log(sum(100))
+// console.log(sum(100))
