38d1dde9b95e8f3c48281cdee64b90ca9487e0cc
Zach Eggert <egger0a6@email.arizona.edu>
2023-06-01 23:16:28 -0400

    add regex to handle options

--- 5kyu/haskell_list_dot_notation.js
+++ 5kyu/haskell_list_dot_notation.js
@@ -1,6 +1,18 @@
 function ArrayComprehension(options) {
+  let array;
   console.log(options.generator)
-  options = options.generator.split(/([,])|([.]+)/).filter(term => term);
+  let regex = /(^-*\d*(?:,*-*\d*)*)|([.][.])/;
+  // options = options.generator.matchAll(regex);
+  // for (const match of options) {
+  //   console.log(match)
+  //   console.log()
+  // }
+  options = options.generator.split(regex).filter(term => term);
   console.log(options)
   return [];
 }
+
+console.log(ArrayComprehension({generator: '1,4,-3'}))
+console.log(ArrayComprehension({generator: ""}))
+console.log(ArrayComprehension({generator: "1"}))
+console.log(ArrayComprehension({generator: "1,3..7"}))
