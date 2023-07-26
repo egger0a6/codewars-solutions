779c52d04d17aaa7ff527e2d25680e1f06dab99b
Zach Eggert <egger0a6@email.arizona.edu>
2023-07-26 14:24:14 -0400

    change solution to different method of solving

--- 5kyu/haskell_list_dot_notation.js
+++ 5kyu/haskell_list_dot_notation.js
@@ -1,64 +1,35 @@
-function ArrayComprehension(options) {
-  if (!options.generator) return [];
-  console.log()
-  let array;
-  console.log(options.generator)
-  let regex = /(\.\.)/;
-  // let regex = /(^-*\d*(?:,*-*\d*)*)|(\.\.)/;
-  options = options.generator.split(regex);
-  console.log(options)
-  if (options.includes("..")) {
-    if (options.length > 3) {
-      array = [];
+function ArrayComprehension({generator = ""}) {
+  if (!generator) return [];
+  let regex = /(-?\d+)\.\.(-?\d+)|(-?\d+),(-?\d+)\.\.(-?\d+)/;
+  let options = generator.match(regex);
+  if (options) {
+    let array = [];
+    options = options.map(Number).filter(x => !isNaN(x));
+    let start, step, end;
+    if (options.length === 2)  {
+      [start, end] = options;
+      if (start > end) return array;
+      step = 1;
     }
     else {
-      let start = options[0].split(",");
-      let end = options[2].split(",");
-      console.log(start)
-      console.log(end)
-      if (start.length > 2 || end.length > 1) {
-        array = [];
-      }
-      else {
-        let step = (start.length > 1) ? +start[1] - +start[0]: 1;
-        console.log(step)
-        start = +start[0];
-        end = +end[0];
-        console.log(start)
-        console.log(end)
-        array = [];
-        if (start < end) {
-          for (let i = start; i <= end; i += step) {
-            array.push(i);
-          }
-        }
-        else  {
-          if (step == 1) {
-            return array;
-          }
-          for (let i = start; i >= end; i += step) {
-            array.push(i);
-          }
-        }
+      [start, step, end] = options;
+      step = step - start;
+    }
+
+    if (start < end) {
+      for (let i = start; i <= end; i += step) {
+        array.push(i);
       }
     }
+    else {
+      for (let i = start; i >= end; i += step) {
+        array.push(i);
+      }
+    }
+
+    return array;
   }
   else {
-    array = options.length ? options[0].replaceAll(" ", "").split(",").filter(el => el).map(el => +el) : [];
+    return JSON.parse(`[${generator}]`);
   }
-  return array;
-} 
-
-console.log(ArrayComprehension({generator: ""}))
-console.log(ArrayComprehension({generator: "1"}))
-console.log(ArrayComprehension({generator: '2,3,-5,3'}))
-console.log(ArrayComprehension({generator: "1,3..7"}))
-console.log(ArrayComprehension({generator: "1..5"}))
-console.log(ArrayComprehension({generator: "6,5..3"}))
-console.log(ArrayComprehension({generator: "6,4..0"}))
-console.log(ArrayComprehension({generator: "5..3"}))
-console.log(ArrayComprehension({generator: "10,1..10"}))
-// console.log(ArrayComprehension({generator: "1,1..10"}))
-console.log(ArrayComprehension({generator: "1..9,12..15"}))
-console.log(ArrayComprehension({generator: "1,2..20,25"}))
-console.log(ArrayComprehension({generator: "1,2,3..20"}))
+}
