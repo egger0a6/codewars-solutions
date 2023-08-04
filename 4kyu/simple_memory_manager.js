3692fff1747f9b8f3a993db2d47e3aa99de0c5da
Zach Eggert <egger0a6@email.arizona.edu>
2023-08-03 23:45:24 -0400

    update how values are stored when using the write method

--- 4kyu/simple_memory_manager.js
+++ 4kyu/simple_memory_manager.js
@@ -5,13 +5,13 @@ class MemoryManager {
    */
       constructor(memory) {
         this.memory = memory;
+        this.blocks = new Array(memory.size);
         this.free = memory.length;
-        this.memory[0] = {
+        this.blocks[0] = {
           size: memory.length,
           free: true,
           prev: null,
           next: null,
-          data: null,
         }
       }
   /**
@@ -26,8 +26,8 @@ class MemoryManager {
         }
         else {
           let i = 0;
-          while (i < this.memory.length) {
-            let currBlock = this.memory[i];
+          while (i < this.blocks.length) {
+            let currBlock = this.blocks[i];
             if (currBlock.free) {
               if (size < currBlock.size) {
                 let newBlock = {
@@ -35,18 +35,16 @@ class MemoryManager {
                   free: false,
                   prev: currBlock.prev,
                   next: i + size,
-                  data: new Array(size),
                 }
-                this.memory[i] = newBlock;
+                this.blocks[i] = newBlock;
                 currBlock.size -= size;
                 currBlock.prev = i;
-                this.memory[i+size] = currBlock;
+                this.blocks[i+size] = currBlock;
                 this.free -= size;
                 return i;
               }
               else if (size === currBlock.size) {
                 currBlock.free = false;
-                currBlock.data = new Array(currBlock.size);
                 this.free -= size;
                 return i;
               }
@@ -56,7 +54,7 @@ class MemoryManager {
               }
             }
             else {
-              i += this.memory[i].size;
+              i += this.blocks[i].size;
               continue;
             }
           }
@@ -69,26 +67,24 @@ class MemoryManager {
    * @throws If the pointer does not point to an allocated block.
    */
       release(pointer) {
-        if (this.memory[pointer] && !this.memory[pointer].free) {
-          let freeBlock = this.memory[pointer];
+        if (this.blocks[pointer] && !this.blocks[pointer].free) {
+          let freeBlock = this.blocks[pointer];
           freeBlock.free = true;
-          freeBlock.data = null;
           this.free += freeBlock.size;
-          if (freeBlock.next && this.memory[freeBlock.next].free) {
+          if (freeBlock.next && this.blocks[freeBlock.next].free) {
             let next = freeBlock.next;
-            freeBlock.size += this.memory[next].size;
-            freeBlock.next = this.memory[next].next;
-            this.memory[next] = null;
+            freeBlock.size += this.blocks[next].size;
+            freeBlock.next = this.blocks[next].next;
+            this.blocks[next] = null;
           }
-          if (freeBlock.prev && this.memory[freeBlock.prev].free) {
+          if (freeBlock.prev && this.blocks[freeBlock.prev].free) {
             let prev = freeBlock.prev;
-            if (this.memory[freeBlock.next]) {
-              this.memory[freeBlock.next].prev = prev;
+            if (this.blocks[freeBlock.next]) {
+              this.blocks[freeBlock.next].prev = prev;
             }
-            this.memory[prev].size += freeBlock.size;
-            this.memory[prev].next = freeBlock.next;
-            //this.memory[prev].data = null;
-            this.memory[pointer] = null;
+            this.blocks[prev].size += freeBlock.size;
+            this.blocks[prev].next = freeBlock.next;
+            this.blocks[pointer] = null;
           }
         }
         else {
@@ -104,13 +100,13 @@ class MemoryManager {
       read(pointer) {
         let i = pointer;
         let ptr = 0;
-        while(!this.memory[i]) {
+        while(!this.blocks[i]) {
           i--;
           ptr++;
         }
-        if (!this.memory[i].free) {
-          if (this.memory[i].data[ptr]) {
-            return this.memory[i].data[ptr];
+        if (!this.blocks[i].free) {
+          if (this.blocks[i].data[ptr]) {
+            return this.blocks[i].data[ptr];
           }
           else {
             return undefined;
@@ -128,13 +124,13 @@ class MemoryManager {
    */
       write(pointer, value) {
         let i = pointer;
-        let ptr = 0;
-        while (!this.memory[i]) {
+        while (!this.blocks[i]) {
           i--;
-          ptr++;
         }
-        if (!this.memory[i].free) {
-          this.memory[i].data[ptr] = value;
+        if (!this.blocks[i].free && this.blocks[i].next) {
+          if (pointer < this.blocks[i].next) {
+            this.memory[pointer] = value;
+          }
         }
         else {
           throw new Error(`no memory has been allocated`);
@@ -173,7 +169,9 @@ pointer2 = mem.allocate(16);
 let pointer3 = mem.allocate(16);
 let pointer4 = mem.allocate(16);
 mem.release(pointer3);
+console.log(mem)
 mem.release(pointer2);
+console.log(mem)
 mem.allocate(32);
 console.log(mem)
 
@@ -190,13 +188,10 @@ d = 32;
 mem = new MemoryManager(array);
 pointer1 = mem.allocate(32);
 mem.write(pointer1, a);
-// TODO make data property an array of size of the block so indeces within that
-// TODO block can be written to
 mem.write(pointer1 + b, b);
 mem.write(pointer1 + c, c);
 //mem.write(pointer1 + d);
 console.log(mem)
-console.log(mem.memory[0].data)
 
 // May not read from unallocated blocks
 mem = new MemoryManager(new Array(64));
@@ -208,3 +203,149 @@ pointer1 = mem.allocate(32);
 mem.write(pointer1, 1)
 console.log(mem.read(pointer1));
 console.log(mem.read(pointer1 + 1));
+
+
+// Old solution ================================================================
+
+// class MemoryManager {
+//   /**
+//    * @constructor Creates a new memory manager for the provided array.
+//    * @param {memory} An array to use as the backing memory.
+//    */
+//       constructor(memory) {
+//         this.memory = memory;
+//         this.free = memory.length;
+//         this.memory[0] = {
+//           size: memory.length,
+//           free: true,
+//           prev: null,
+//           next: null,
+//           data: null,
+//         }
+//       }
+//   /**
+//    * Allocates a block of memory of requested size.
+//    * @param {number} size - The size of the block to allocate.
+//    * @returns {number} A pointer which is the index of the first location in the allocated block.
+//    * @throws If it is not possible to allocate a block of the requested size.
+//    */
+//       allocate(size) {
+//         if (size > this.free) {
+//           throw new Error(`cannot allocate more memory than exists`);
+//         }
+//         else {
+//           let i = 0;
+//           while (i < this.memory.length) {
+//             let currBlock = this.memory[i];
+//             if (currBlock.free) {
+//               if (size < currBlock.size) {
+//                 let newBlock = {
+//                   size: size,
+//                   free: false,
+//                   prev: currBlock.prev,
+//                   next: i + size,
+//                   data: new Array(size),
+//                 }
+//                 this.memory[i] = newBlock;
+//                 currBlock.size -= size;
+//                 currBlock.prev = i;
+//                 this.memory[i+size] = currBlock;
+//                 this.free -= size;
+//                 return i;
+//               }
+//               else if (size === currBlock.size) {
+//                 currBlock.free = false;
+//                 currBlock.data = new Array(currBlock.size);
+//                 this.free -= size;
+//                 return i;
+//               }
+//               else {
+//                 i += currBlock.size;
+//                 continue;
+//               }
+//             }
+//             else {
+//               i += this.memory[i].size;
+//               continue;
+//             }
+//           }
+//           throw new Error(`cannot allocate more memory than available`);
+//         }
+//       }
+//   /**
+//    * Releases a previously allocated block of memory.
+//    * @param {number} pointer - The pointer to the block to release.
+//    * @throws If the pointer does not point to an allocated block.
+//    */
+//       release(pointer) {
+//         if (this.memory[pointer] && !this.memory[pointer].free) {
+//           let freeBlock = this.memory[pointer];
+//           freeBlock.free = true;
+//           freeBlock.data = null;
+//           this.free += freeBlock.size;
+//           if (freeBlock.next && this.memory[freeBlock.next].free) {
+//             let next = freeBlock.next;
+//             freeBlock.size += this.memory[next].size;
+//             freeBlock.next = this.memory[next].next;
+//             this.memory[next] = null;
+//           }
+//           if (freeBlock.prev && this.memory[freeBlock.prev].free) {
+//             let prev = freeBlock.prev;
+//             if (this.memory[freeBlock.next]) {
+//               this.memory[freeBlock.next].prev = prev;
+//             }
+//             this.memory[prev].size += freeBlock.size;
+//             this.memory[prev].next = freeBlock.next;
+//             this.memory[pointer] = null;
+//           }
+//         }
+//         else {
+//           throw new Error(`no memory has been allocated`);
+//         }
+//       }
+//   /**
+//    * Reads the value at the location identified by pointer
+//    * @param {number} pointer - The location to read.
+//    * @returns {number} The value at that location.
+//    * @throws If pointer is in unallocated memory.
+//    */
+//       read(pointer) {
+//         let i = pointer;
+//         let ptr = 0;
+//         while(!this.memory[i]) {
+//           i--;
+//           ptr++;
+//         }
+//         if (!this.memory[i].free) {
+//           if (this.memory[i].data[ptr]) {
+//             return this.memory[i].data[ptr];
+//           }
+//           else {
+//             return undefined;
+//           }
+//         }
+//         else {
+//           throw new Error(`no memory has been allocated`);
+//         }
+//       }
+//   /**
+//    * Writes a value to the location identified by pointer
+//    * @param {number} pointer - The location to write to.
+//    * @param {number} value - The value to write.
+//    * @throws If pointer is in unallocated memory.
+//    */
+//       write(pointer, value) {
+//         let i = pointer;
+//         let ptr = 0;
+//         while (!this.memory[i]) {
+//           i--;
+//           ptr++;
+//         }
+//         if (!this.memory[i].free) {
+//           this.memory[i].data[ptr] = value;
+//         }
+//         else {
+//           throw new Error(`no memory has been allocated`);
+//         }
+//       }
+//   }
