3136e23ba70f06ce3d2c845e7ad511898adb6ecf
Zach Eggert <egger0a6@email.arizona.edu>
2023-08-06 17:46:57 -0400

    add function for binomial coefficient, complete working solution

--- 4kyu/total_increasing_or_decreasing_numbers_up_to_power_of_10.js
+++ 4kyu/total_increasing_or_decreasing_numbers_up_to_power_of_10.js
@@ -1,5 +1,23 @@
 // Solution for 4kyu Kata https://www.codewars.com/kata/55b195a69a6cc409ba000053/train/javascript
 
 function totalIncDec(x){
-  //your code here
+  x = BigInt(x);
+  let n = 10n;
+  let inc = binomCoeff(x + n - 1n, n - 1n) - 1n;
+  let dec = binomCoeff(x + n, n);
+
+  return inc + dec - n * x;
+}
+
+
+// calc binomial coefficient (n choose k)
+function binomCoeff(n, k) {
+  let coeff = 1n;
+
+  for (let i = 0n; i < k; i++) {
+    coeff *= n - i;
+    coeff /= i + 1n;
+  }
+
+  return coeff;
 }
