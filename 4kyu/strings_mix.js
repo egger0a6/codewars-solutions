48c6359f563952851ec32c4e7208fa7a548192e4
Zach Eggert <egger0a6@email.arizona.edu>
2023-08-10 18:46:17 -0400

    refactor solution

--- 4kyu/strings_mix.js
+++ 4kyu/strings_mix.js
@@ -1,45 +1,14 @@
 // Solution for 4kyu Kata "Strings Mix" https://www.codewars.com/kata/5629db57620258aa9d000014/train/javascript
 
 function mix(s1, s2) {
-  let s1Count = new Array(26), s2Count = new Array(26), mix = [];
-  let regex = /[a-z]/g;
+  let s1Count, s2Count, mix = [];
 
-  s1 = s1.match(regex)
-  .forEach(c => {
-    let aPos = c.charCodeAt(0) - 97;
-    if (s1Count[aPos]) {
-      s1Count[aPos] += c;
-    }
-    else {
-      s1Count[aPos] = c;
-    }
-  });
-  s2 = s2.match(regex)
-  .forEach(c => {
-    let aPos = c.charCodeAt(0) - 97;
-    if (s2Count[aPos]) {
-      s2Count[aPos] += c;
-    }
-    else {
-      s2Count[aPos] = c;
-    }
-  });
+  s1Count = countSortedLetters(s1);
+  s2Count = countSortedLetters(s2);
 
-  s1Count = s1Count
-  .map(el => {
-    if (!el || el.length < 2) {
-      return null;
-    }
-    return el;
-  });
-  s2Count = s2Count
-  .map(el => {
-    if (!el || el.length < 2) {
-      return null;
-    }
-    return el;
-  });
-  
+  s1Count = filterByLength(s1Count);
+  s2Count = filterByLength(s2Count);
+
   for (let i = 0; i < s1Count.length; i++) {
     s1Chars = s1Count[i];
     s2Chars = s2Count[i];
@@ -54,8 +23,8 @@ function mix(s1, s2) {
     if (s1Chars) mix.push(`1:${s1Chars}`);
     if (s2Chars) mix.push(`2:${s2Chars}`);
   }
+  
   mix.sort((a, b) => b.length - a.length);
-
   mix.sort((a, b) => {
     if (a.length === b.length) {
       if (a.at(0) === "=" || b.at(0) === "=") {
@@ -66,4 +35,28 @@ function mix(s1, s2) {
     return 0;
   });
   return (mix) ? mix.join("/") : "";
+}
+
+function countSortedLetters(str) {
+  let count = new Array(26), regex = /[a-z]/g;;
+  str.match(regex)
+  .forEach(c => {
+    let aPos = c.charCodeAt(0) - 97;
+    if (count[aPos]) {
+      count[aPos] += c;
+    }
+    else {
+      count[aPos] = c;
+    }
+  });
+  return count;
+}
+
+function filterByLength(arr) {
+  return arr.map(el => {
+    if (!el || el.length < 2) {
+      return null;
+    }
+    return el;
+  });
 }
