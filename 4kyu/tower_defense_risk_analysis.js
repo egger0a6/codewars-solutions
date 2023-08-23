64b4147e3022ac24ea9fbaca72f322ee43e8d6fa
Zach Eggert <egger0a6@email.arizona.edu>
2023-08-23 14:53:40 -0400

    update partial solution

--- 4kyu/tower_defense_risk_analysis.js
+++ 4kyu/tower_defense_risk_analysis.js
@@ -1,17 +1,25 @@
 // Solution to 4kyu Kata "Tower Defense: Risk Analysis" https://www.codewars.com/kata/5a57faad880385f3b60000d0/train/javascript
 
 
+let globalN;
+let globalLen;
+let turrPositions = {};
 const PATH = [];
 
 function towerDefense(grid,turrets,aliens){
 	setup(grid, turrets);
+  console.log(PATH)
+  console.log(turrets)
+  return mainLoop(turrets, aliens);
 }
 
 
 // first pass through the grid to setup data structures
 function setup(grid, turrets) {
+  globalN = grid.length;
   const N = grid.length;
   grid = grid.reduce((prev, curr) => prev + curr, "");
+  globalLen = grid.length;
   let start = grid.indexOf("0");
 
   // find grid path and store in global array PATH
@@ -21,19 +29,11 @@ function setup(grid, turrets) {
   for (let i = 0; i < grid.length; i++) {
     let currChar = grid.at(i);
     if (/[A-Z]/.test(currChar)) {
+      turrPositions[currChar] = i;
       turrets[currChar].push(i);
       getTurretTargets(currChar, turrets, N);
     }
   }
-
-  // calculate total shots of all turrets
-  let totalShots = Object.keys(turrets).reduce((prev, curr) => {
-    return prev + turrets[curr].totShots;
-  }, 0);
-
-  console.log(PATH)
-  console.log(turrets)
-  return mainLoop(turrets, aliens, totalShots);
 }
 
 function mainLoop(turrets, aliens) {
@@ -42,13 +42,13 @@ function mainLoop(turrets, aliens) {
 
   state[0] = aliens[0];
   while (true) {
-    console.log(state)
+    printState(state, turrets);
     for(let i = state.length - 1; i >= 0; i--) {
       if (state[i]) {
         validTurrets = [];
 
         // determine turrets that are able to shoot current alien and total
-        // available shots that said turret has in the current move.
+        // available shots that said turrets have in the current move.
         validShots = Object.keys(turrets).reduce((prev, curr) => {
           if (turrets[curr].shotRange.includes(PATH[i])) {
             validTurrets.push(curr);
@@ -69,6 +69,7 @@ function mainLoop(turrets, aliens) {
                 totAliens--;
                 break;
               }
+              console.log(turret)
               console.log(state[i])
             }
           }
@@ -105,7 +106,7 @@ function mainLoop(turrets, aliens) {
       break;
     }
   }
-  console.log(totHealthPoints)
+  return totHealthPoints;
 }
 
 // get indexes of path. grid is now a single string reduced from the original
@@ -133,11 +134,10 @@ function getPath(grid, start, N) {
       hasNeighbor = false;
     }
   }
-  // PATH.sort((a, b) => a - b);
 }
 
 // calculate the path indices that are in range of the turret, t.
-// uses Manhattan Distance
+// uses Manhattan Distance: |x1 - x2| + |y1 - y2|
 function getTurretTargets(t, turrets, N) {
   let tPos = turrets[t][2], tRange = turrets[t][0];
   let tRow = Math.floor(tPos / N), tCol = tPos % N, pRow, pCol, distance;
@@ -154,5 +154,37 @@ function getTurretTargets(t, turrets, N) {
     totShots: turrets[t][1],
     remShots: turrets[t][1],
     shotRange: tTargets.reverse(),
+  }
+}
+
+// prints state for debugging
+function printState(state) {
+  let turrLocations = Object.keys(turrPositions).reduce((prev, curr) => {
+    return [...prev, turrPositions[curr]]
+  }, []);
+  let currRow = "";
+
+  for (let i = 0; i < globalLen; i += globalN) {
+    for (let j = i; j < i + globalN; j++) {
+      if (turrLocations.includes(j)) {
+        for (turr in turrPositions) {
+          if (turrPositions[turr] === j) {
+            currRow += " " + turr + " ";
+          }
+        }
+      }
+      else if (PATH.includes(j) && !state[PATH.indexOf(j)]) {
+        currRow += " " + 0 + " ";
+      }
+      else if(PATH.includes(j) && state[PATH.indexOf(j)]) {
+        let alien = state[PATH.indexOf(j)];
+        currRow += (alien > 9) ? alien + " " : " " + alien + " ";
+      }
+      else {
+        currRow += " - ";
+      }
+    }
+    console.log(currRow);
+    currRow = "";
   }
 }
