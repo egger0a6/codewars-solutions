5c9d1945c88463ad46161d963fb4825302505305
Zach Eggert <egger0a6@email.arizona.edu>
2025-01-23 15:00:03 -0500

    add 7kyu solution

---
+++ 7kyu/milk_and_cookies_for_santa.js
@@ -0,0 +1,5 @@
+// Solution for 7kyu Kata "Milk and Cookies for Santa" https://www.codewars.com/kata/52af7bf41f5a1291a6000025
+
+function timeForMilkAndCookies(date){
+  return /Dec 24/.test(date.toString());
+}
